28a95caa1cc3b9942e3f8fae8cbafdfa0293a185
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-16 10:53:57 +0200

    setup

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+
+class App extends React.Component {
+
+    state = {resource: 'posts'}
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <button onClick={() => this.setState({resource:'Posts'})}>Posts</button>
+                    <button onClick={() => this.setState({resource:'Todos'})}>Todos</button>
+
+                </div>
+                {this.state.resource}
+            </div>
+        )
+    }
+}
+
+export default App
