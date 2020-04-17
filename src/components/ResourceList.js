46908bdf6eeac2fc70d79fdfd67c240bf1034d89
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-17 11:31:35 +0200

    useEffect, changing from class to func. comp.

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
+// class ResourceList extends React.Component {
 
-    state = { resource: [] };
+    // state = { resource: [] };
     
-    async componentDidMount(){
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    // async componentDidMount(){
+    //     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    //     this.setState({resource: response.data})
+    // }
 
-        this.setState({resource: response.data})
+    //preProps(not important name) are the props from the moment, before the componenetDidUpdate is run
+    // async componentDidUpdate(preProps){
+    //     if(preProps.resource !== this.props.resource){
+    //         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    //         this.setState({resource: response.data})
+    //     }
+    // }
+
+  const ResourceList = ({resource}) => {
+
+    const [resources,setResource] = useState([]);
+
+    const fetchResource = async(resource) =>{
+        // const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+        // this.setState({resource: response.data})
+        setResource(response.data)
     }
 
-    //preProps are the props from the moment, before the componenetDidUpdate is run
-    async componentDidUpdate(preProps){
-        if(preProps.resource !== this.props.resource){
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-            this.setState({resource: response.data})
-        }
-    }
+    useEffect(()=> {
+        fetchResource(resource)
+    }, [resource])
 
-
-
-    render() { 
+    // render() { 
         return (
             <div>
-                {this.state.resource.length}
+                {/* {this.state.resource.length} */}
+                {resources.length}
+
             </div>
         )
-    }
+    // }
 }
 
 export default ResourceList;
